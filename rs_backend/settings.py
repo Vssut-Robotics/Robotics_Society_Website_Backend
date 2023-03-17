11f3681b5a5f6f6acf3e1178ebcc88763794d89e
rajat-gith <rajatnayak1582002@gmail.com>
2023-03-17 22:31:19 +0530

    Media Files worked on

--- rs_backend/settings.py
+++ rs_backend/settings.py
@@ -126,7 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR/'static'
 ]
