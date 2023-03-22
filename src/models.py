6e935735c0a39abb435422e7cfff399e51d06e68
rajat-gith <rajatnayak1582002@gmail.com>
2023-03-23 00:28:16 +0530

    c

--- src/models.py
+++ src/models.py
@@ -25,6 +25,9 @@ class projects(models.Model):
     members = models.ManyToManyField(team_member, blank=True, null=True)
     ytlink = models.CharField(max_length=200, null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class member(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
@@ -39,3 +42,6 @@ class member(models.Model):
     twitterLink = models.CharField(max_length=200, null=True, blank=True)
     projects_part = models.ManyToManyField(projects, blank=True, null=True)
     profile = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
