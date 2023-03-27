3488405f4a04ffce389f4f676e8077cf0f07dd58
rajat-gith <rajatnayak1582002@gmail.com>
2023-03-27 10:51:17 +0530

    Cors Update

--- rs_backend/settings.py
+++ rs_backend/settings.py
@@ -79,7 +79,8 @@ WSGI_APPLICATION = 'rs_backend.wsgi.application'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1"
+    "http://127.0.0.1",
+    "https://robotics-society-frontend.vercel.app/"
     
 ]
 
